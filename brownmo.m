3f6fe0f8d90eb496e9c56499cd3d48baf5676c20
Matthias <66843529+matthias-10@users.noreply.github.com>
2021-11-06 09:23:08 +0100

    C, quelques graphiques

--- brownmo.m
+++ brownmo.m
@@ -1,86 +1,108 @@
-tic
 
-r = 2;
-sigma = 0.2;
-P0 = 40;
 
-T = 1/12;
+r = 0.02;
+sigma = 0.02;
+S0 = 40;
+
+T = 100;
 t0=0;
-n=2^5;
+n=2^8;
 dt = ((T-t0)/n);
 t = t0:dt:T;
 
-nt = 1;
-
-
-% n trajectoires
-% plot(P0); %pour effacer le plot
-% hold on %pour faire plusieurs plots
-% x_ax = t; % x-axe
-% 
-% axis([0 T P0+r*(-2) P0+r*8]); %x-axe limits
-% 
-% for i = 1:nt
-%     plot(x_ax, brownmo(P0, r, sigma ,t0, T, n)) %brownmo est definie en bas
-% end
+t_mm = n;
+nt = 10; % nombre des trajectoires
 
 
 
+tic
 
-duree= toc; % Matthias HPPavilion: ~ 0.22, le script R: ~ 0.09 secs; nt=30 n=2^8
-disp(sprintf('%d trajectoires plote', nt));
-disp(sprintf('Fini en %0.5g', duree));
-
-% nouvelle simulation en forme matrice:
+% simulation en forme matrice:
 
 S_sim = zeros(length(t),nt);
 for i = 1:nt
-    S_sim(:,i) = brownmo(P0, r, sigma ,t0, T, n); %brownmo est definie en bas
+    S_sim(:,i) = brownmo(S0, r, sigma ,t0, T, n); %brownmo est definie en bas
 end
+
+duree= toc; 
+% Matthias HPPavilion: ~ 0.22, le script R: ~ 0.09 secs; nt=30 n=2^8
+fprintf('%d trajectoires plotees\n', nt);
+fprintf('Fini en %0.5g\n', duree);
+
 
 % plot
-plot(P0); %pour effacer le plot
-hold on %pour faire plusieurs plots
+tiledlayout(3,1)
+
 x_ax = t; % x-axe
 
-axis([0 T P0+r*(-2) P0+r*8]); %x-axe limits
+axis([0 T 0.9*min(min(S_sim)) 1.1*max(max(S_sim))]) %x-axe limits
+%axis([0 T S0-sigma*n S0*(1+r)^(T-t0)+sigma*n]); 
 
-for i = 1:nt
-    plot(x_ax,S_sim(:,i));
-    %plot(x_ax, brownmo(P0, r, sigma ,t0, T, n)) 
-end
+nexttile
+hold on
+% pour comparison, si j'epargne pour le taux r:
+plot([t0 T], [S0 S0*(1+r)^(T-t0)], "--k"); % obligation
+plot(x_ax,S_sim);
+
 
 % calculer X_T
- % au lieu de l'integrale cumsum
+ % cumsum au lieu de l'integrale
 
  %X = [0 1 2; 3 4 5];cumsum(X,1)
 
-X_T = cumsum(S_sim,1);
-X_T = X_T(n+1,:);
-X_T = X_T/T;
+% X_T = cumsum(S_sim,1);
+% X_T = X_T(n+1,:);
+% X_T = X_T/T;
 
 % calculer X_t
-
-
-plot(x_ax(1:(20+1)),MoyMob(S_sim, 20), ':');
 hold off
-% calculer le prix des options
-MoyMob(S_sim, 20)
-size(MoyMob(S_sim, 20))
+nexttile
+hold on
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%% fonctions %%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+plot([t0 T], [S0 S0*(1+r)^(T-t0)], "--k"); % obligation
+plot(x_ax(1:(t_mm+1)),MoyMob(S_sim, t_mm), ':');
+plot(x_ax,S_sim);
 
-function S = brownmo(X0, mu, sigma, t0, t, n) %x0 
+%hold off
 
-  delta = (t-t0)/n;
+%MoyMob(S_sim, t_mm)
+%size(MoyMob(S_sim, t_mm))
+
+% calculer K = S_0 * exp(rT)
+
+K = S0*exp(r*T);
+
+% calculer le prix de l'option C_t = max(0, X_t-S_t)
+Xn = MoyMob(S_sim, n);
+
+hold off
+nexttile
+hold on
+
+plot(x_ax, Xn-S_sim);
+plot([t0 T], [0 0], ":k"); % zero
+
+hold off
+nexttile
+
+C = ( (Xn - S_sim) >= 0 ) .* (Xn - S_sim);
+
+
+plot(x_ax, C); % Il y a trop de valeurs >0 ? r?
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%% fonctions %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function S = brownmo(X0, mu, sigma, t0, T, n) %x0 
+
+  delta = (T-t0)/n;
   W = zeros(1,n+1);
-  tseq = t0:((t-t0)/n):t;
+  tseq = t0:((T-t0)/n):T;
   for i = 2:(n+1)
     W(i) = W(i-1)+normrnd(0,1)*sqrt(delta);
   end
-  S = X0 * exp((mu-(sigma^2)/2)*(tseq-t0)+sigma*W);
+  S = X0 * exp( (mu-(sigma^2)/2) * (tseq-t0) + sigma*W );
 
 end
 
@@ -90,6 +112,14 @@ function X_t = MoyMob(M, t_m)
     t_m2= t_m+1;
     X_t = X_t(1:t_m2,:);
     for i = 1:t_m2
-        X_t(i,:) = X_t(i,:)/i;
+        X_t(i,:) = X_t(i,:)/i; %vectoriel?
     end
 end
+
+
+
+
+
+
+
+
