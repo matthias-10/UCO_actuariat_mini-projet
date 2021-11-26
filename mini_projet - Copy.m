4c95823db1dc576118383afaf03429267952cec5
Matthias Lang <matthiaslang96@gmail.com>
2021-11-26 23:43:19 +0100

    matlab fini, graphiques sauvegardees

--- mini_projet - Copy.m
+++ mini_projet - Copy.m
@@ -5,15 +5,17 @@
 % Matthias LANG                   %
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
 % ~~~~~ Mathematiques financieres: Mini-projet 1 ~~~~~~~~ %
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
 
 %% ~~~~~~~~~~~~~~~~~~~~ Parametres ~~~~~~~~~~~~~~~~~~~~~ %%
 
 S0 = 40;                % Prix initial du sous jacent
-K = 41;                 % Prix d'exercice de l'option
+K = 42;                 % Prix d'exercice de l'option
 
 r = 0.05;               % Taux d'interet sous risque neutre
-sigma = 0.01;           % Variance partie fixe
+sigma = 0.01/sqrt(S0);  % Variance partie fixe
 
 t0 = 0;                 % Debut de la periode
 n = 2^9;                % Nombre de intervalles
@@ -22,9 +24,7 @@ Nd = 8;                 % Nombre des sous-intervalles
 
 nt = 1000;              % Nombre de trajectoires
 
-
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %%
-
 if Nd > n/2-1
     warning("Le nombre des sous-intervalles est trop petit")
     fprintf('Il fallait Nd << n')
@@ -68,15 +68,13 @@ C_inf = zeros(1,nt);
 C_N = zeros(1,nt);
 for j = 1:nt
     S_vec = S(:,j);
-   
-
-    %% ~~~~~~~~~~~~ C_inf: calcul avec X_T ~~~~~~~~~~~~~ %%
+    %% ~~~~~~~~~~~~~~~ calcul avec X_t ~~~~~~~~~~~~~~~~~ %%
 
     % integral: l'aire de t0 a T sous S
     X_T = 0.5*S0 + sum(S_vec(2:n,:),1) + 0.5*S_vec(n+1,:);
     X_T = X_T/n; %ou (n+1)?
 
-    C_inf_j = (X_T - K) .* ( X_T - K >= 0 );
+    C_inf_j = X_T - K .* ( X_T - K >= 0 );
     C_inf_0 = exp(-r*T)*C_inf_j;
 
     % ~ Estimateur ~
@@ -85,8 +83,7 @@ for j = 1:nt
     
     C_inf(j)=C_inf_0;
 
-
-    %% ~~~~~~~~~~ C_N: calcul avec X_T_prim ~~~~~~~~~~~~ %%
+    %% ~~~~~~~~~~~~~ calcul avec X_t_prim ~~~~~~~~~~~~~~ %%
 
     %1/N * sum_1^N S_{kT/N}
     % => kT n'est pas un numero entier, il faut arrondir
@@ -98,7 +95,7 @@ for j = 1:nt
     index = index(1:(n/Nd):end); 
     X_T_prim = sum(S_vec(index,:),1)/Nd;
 
-    C_N_j = (X_T_prim - K) .* ( X_T_prim - K >= 0 );
+    C_N_j = X_T_prim - K .* ( X_T_prim - K >= 0 );
 
     % C_N * exp(-rT) est une martingale donc 
     % E[exp(-rT)*C_N]= C_N(S_0)
@@ -107,7 +104,6 @@ for j = 1:nt
     C_N(j)=C_N_0;
 
 end
-
 
 %% ~~~~~~~~~~~~ affichage des estimateurs ~~~~~~~~~~~~~~ %%
 
@@ -138,88 +134,84 @@ fprintf(['L''estimateur du C_N a t0, avec ' ...
     '%d sous-intervalles = \n%0.5g\n'], ...
     Nd, C_N_est_mat);
 fprintf('Son ecart type = %0.5g\n', sqrt(C_N_est_var));
-
-
 %% ~~~~~~~~~~~~~~~~~~~~~ graphes ~~~~~~~~~~~~~~~~~~~~~~~ %%
 
 % 1:   graphe de S; 
 % 2-3: histogrammes de C_inf et C_N; 
 % 4-5: boxplot des estimateurs
 
-G = "g";
-P = input(['\n' ...
-    'Pour afficher n''importe quelle graphique, tapez ' ...
-    'son numero <1-5> ou [Enter]. \n' ...
-    'Pour quitter tapez plusieures fois [Enter]:\n'] );
-
-if isstring(P) || isempty(P)
-    P = 1;
-else 
-    if ~ismember(P,1:6)
-        P = 1;
-    end
-end
-
+P = input( ...
+    ['Pour afficher n''importe quelle graphique, tapez' ...
+    'son numero <1-5>. \n' ...
+    'Pour quitter tapez [q] ou plusieures fois [Enter]'] );
 while G~="q"
-    disp("[Enter] pour continuer")
-    switch P
+P = input(' ');
+switch P
     case 1
-        fprintf('< 1: graphe de S >\n')
-        figure(1)
-        plot([t0 T],[K K], ':k', 'LineWidth',2)
-        hold on
-        plot(t, S)
-        plot([t0 T],[K K], ':k', 'LineWidth',2)
-        hold off
-        % pour comparison, si j'epargne pour le taux r:
-        %plot([t0 T], [S0 S0*(1+r)^(T-t0)], "--k"); %obl.
-        %1% fplot(obligation, [t0 T], "-k"); 
-        legend("K, le prix d''exercice", ...
-               "les prix S_t des actions",...
-               "Location","northwest");
-        P=P+1; input('\n');
-
-    case 2
-        fprintf('< 2: histogramme de C_inf >\n')
-        figure(1)
-        % E_\pi (e^-rT (X_T - K)^+ / F_O) ~ 1/nt \sum {C(T)}
-        histogram( C_inf );
-        title("Histogramm des C(T) pour X_{infinie}");
-        P=P+1; input('\n');
-
-    case 3
-        fprintf('< 3: histogramme de C_N >\n')
-        figure(1)
-        histogram( C_N );
-        title("Histogramm des C(T) pour X_{N}");
-        P=P+1; input('\n');
-
-    case 4
-        fprintf(['< 4: boxplot de l''estimateur ' ...
-                 'C_{infinie} >\n'])
-        figure(1)
-        boxplot( C_inf );
-        title('boxplot de C_{infinie} a T')
-        ylabel('C_T, valeurs actualisees')
-        P=P+1; input('\n');
-
-    case 5
-        fprintf('< 5: boxplot de l''estimateur C_{N} >\n\n')
-        figure(1)
-        boxplot ( C_N );
-        title('boxplot de C_{N} a T')
+        disp('Flor')
         P=P+1;
-
+    case 2
+        disp('Circulo')
+        P=P+1;
+    case 3
+        disp('Onda senoidal')
+        P=P+1;
+    case 4
+        disp('Elipse')
+        P=P+1;
+    case 5
+        disp('Nube')
+        P=P+1;
     case 6
-        P=input([' ~ ~ ~ ~ ~ ~\n ' ...
-            'Pour afficher n''importe quelle graphique, ' ...
-            'tapez son numero <1-5> \n']);
+        P=input('')
         if ismember(P, 1:5)
-            fprintf("Vous avez choisi: ")
+            P = P
         else
-            G="q";
+            G="q"
         end
     otherwise
-        G="q";
+        G="q")
     end
 end
+fprintf('\n< 1: graphe de S >\n')
+input('Tapez [Enter] pour afficher le graphe\n')
+
+plot(t, S)
+
+% pour comparison, si j'epargne pour le taux r:
+%plot([t0 T], [S0 S0*(1+r)^(T-t0)], "--k"); % obligation
+%1% fplot(obligation, [t0 T], "-k"); 
+legend("les prix S_t des actions", "sans risque",...
+       "Location","northwest");
+
+
+fprintf('\n< 2: histogramme de C_inf >\n')
+input('Tapez [Enter] pour afficher le graphe\n')
+
+% E_\pi (e^-rT (X_T - K)^+ / F_O) ~ 1/nt \sum {C(T)}
+histogram( C_inf );
+title("Histogramm des C(T) pour X_{infinie}");
+
+
+fprintf('\n< 3: histogramme de C_N >\n')
+input('Tapez [Enter] pour afficher le graphe\n')
+
+histogram( C_N );
+title("Histogramm des C(T) pour X_{N}");
+
+
+fprintf('\nboxplot des estimateurs')
+fprintf('\n< 4: boxplot de l''estimateur C_{infinie} >\n')
+input('Tapez [Enter] pour afficher le graphe\n')
+
+boxplot( C_inf );
+title('boxplot de C_{infinie} a T')
+ylabel('C_T, valeurs actualisees')
+
+
+fprintf('\n< 5: boxplot de l''estimateur C_{N} >\n')
+input('Tapez [Enter] pour afficher le graphe\n')
+
+boxplot ( C_N );
+title('boxplot de C_{N} a T')
+
